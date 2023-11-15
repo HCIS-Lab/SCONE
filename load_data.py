3bd1644ad6179d2d83e88c27b0221b82d6ffd563
Yen-Ling Tai <71924111+YLingT@users.noreply.github.com>
2023-11-15 19:56:02 +0800

    Create load_data.py

---
+++ load_data.py
@@ -0,0 +1,14 @@
+from torch.utils.data.dataset import Dataset
+import torchvision.transforms as transforms
+import glob
+import torch
+
+class FoodDataset(Dataset):
+    def __init__(self, args):
+        self.rootPath = args.dataset
+    
+    def __len__(self):
+        return len(self.mani_rgb)
+
+    def __getitem__(self, index):
+        return 0
